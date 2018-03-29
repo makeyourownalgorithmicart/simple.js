caa6a9c33268031a2b2ad6281a7045168afa8a13
Make Your Own Algorithmic Art <makeyourownalgorithmicart@gmail.com>
2018-03-29 10:14:34 +0100

    example sketch for circle()

---
+++ examples/sketch_circle.js
@@ -0,0 +1,16 @@
+function setup() {
+  createCanvas(800, 600);
+  background('grey');
+  noLoop();
+}
+
+function draw() {
+  
+  // normal ellipse
+  ellipse(300, 300, 200);
+
+  // simpler circle
+  // centred at (500, 300) with radius 100
+  circle(500, 300, 100);
+
+}
