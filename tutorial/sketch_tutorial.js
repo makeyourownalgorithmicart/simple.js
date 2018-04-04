c37e6ed72cc773c62f7f3cd2b44160a258c43fc1
Make Your Own Algorithmic Art <makeyourownalgorithmicart@gmail.com>
2018-04-04 14:55:02 +0100

    initial

--- tutorial/sketch_tutorial.js
+++ tutorial/sketch_tutorial.js
@@ -1,1 +1,19 @@
-placeholder
+/* 
+
+  sketch_tutorial.js
+
+  sketch used for the simple.js tutorial
+  https://github.com/makeyourownalgorithmicart/simple.js/wiki/Tutorial
+
+  Tariq Rashid, 04-04-2018
+
+*/
+
+function setup() {
+  createCanvas(800, 600);
+}
+
+
+function draw() {
+
+}
